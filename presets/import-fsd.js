f8fcf3397fed804daa8565bb60bce507acc999c8
Oleg Putseiko <oleg.putseiko@gmail.com>
2023-11-19 07:06:57 +0400

    fix(import-fsd-preset): amend file path patterns

--- presets/import-fsd.js
+++ presets/import-fsd.js
@@ -1,56 +1,85 @@
 const globAlias = (path) => `@/${path}/**/*`;
 
 /**
- * @typedef { string } Glob
- * @type { { name: string; actualPaths: Glob[]; deprecatedPaths: Glob[] }[] }
+ * @typedef { string } Path - Path compatible with both Glob and RegExp
+ * @type { { name: string; actualPaths: Path[]; deprecatedPaths: Path[] }[] }
  */
 const LAYERS = [
-  { name: 'app', actualPaths: ['app?(s)'], deprecatedPaths: ['core', 'init'] },
+  {
+    name: 'app',
+    actualPaths: ['app', 'apps'],
+    deprecatedPaths: ['core', 'init'],
+  },
   {
     name: 'processes',
-    actualPaths: ['process?(es)'],
-    deprecatedPaths: ['flow?(s)', 'workflow?(s)'],
+    actualPaths: ['process', 'processes'],
+    deprecatedPaths: ['flow', 'flows', 'workflow', 'workflows'],
   },
   {
     name: 'pages',
-    actualPaths: ['page?(s)'],
-    deprecatedPaths: ['screen?(s)', 'view?(s)', 'layout?(s)'],
+    actualPaths: ['page', 'pages'],
+    deprecatedPaths: [
+      'screen',
+      'screens',
+      'view',
+      'views',
+      'layout',
+      'layouts',
+    ],
   },
   {
     name: 'widgets',
-    actualPaths: ['widget?(s)'],
+    actualPaths: ['widget', 'widgets'],
     deprecatedPaths: [],
   },
   {
     name: 'features',
-    actualPaths: ['feature?(s)'],
-    deprecatedPaths: ['component?(s)', 'container?(s)'],
+    actualPaths: ['feature', 'features'],
+    deprecatedPaths: ['component', 'components', 'container', 'containers'],
   },
   {
     name: 'entities',
-    actualPaths: ['entit@(y|ies)'],
-    deprecatedPaths: ['model?(s)'],
+    actualPaths: ['entity', 'entities'],
+    deprecatedPaths: ['model', 'models'],
   },
   {
     name: 'shared',
     actualPaths: ['shared'],
-    deprecatedPaths: ['common', 'lib?(s)'],
+    deprecatedPaths: ['common', 'lib', 'libs'],
   },
 ];
 
-const RELATED_LAYER_NAME_LIST = LAYERS.map(({ name }) => `\`${name}\``).join(
-  ', ',
+const ALL_LAYER_PATHS = LAYERS.flatMap((layer) =>
+  layer.actualPaths.concat(layer.deprecatedPaths),
 );
+const RELATED_LAYER_NAME_LIST = LAYERS.reduce((acc, { name }, index, array) => {
+  if (index === 0) return name;
+  if (index !== array.length - 1) return `${acc}, ${name}`;
+  return `${acc} or ${name}`;
+}, '');
 
 const DEPRECATED_GLOB_ALIASES = LAYERS.flatMap((layer) =>
   layer.deprecatedPaths.map(globAlias),
 );
-const UNKNOWN_GLOB_ALIASES = [
-  `@/!(${LAYERS.flatMap((layer) =>
-    layer.actualPaths.concat(layer.deprecatedPaths).join('|'),
-  )})/**/*`,
+const BREAKING_GLOB_ALIASES = ['/', './', '../'];
+
+const BASE_PATTERNS = [
+  {
+    group: DEPRECATED_GLOB_ALIASES,
+    message: `Layer is deprecated. Instead, use layers related to FSD version 2.X.X: ${RELATED_LAYER_NAME_LIST}`,
+  },
+  {
+    group: BREAKING_GLOB_ALIASES,
+    message:
+      'Using a relative and absolute paths may result in using an inaccessible layers. Use an aliased path instead',
+  },
+  {
+    group: ['@/*'].concat(
+      ALL_LAYER_PATHS.flatMap((path) => [`!@/${path}`, `!@/${path}/*`]),
+    ),
+    message: `Unknown layer, use one related to FSD version 2.X.X: ${RELATED_LAYER_NAME_LIST}`,
+  },
 ];
-const BREAKING_GLOB_ALIASES = ['*(.)/'];
 
 /**
  * Import Feature Sliced Design preset related to FSD up to v2.X.X
@@ -100,33 +129,19 @@ module.exports = {
       prevLayer.actualPaths.concat(prevLayer.deprecatedPaths),
     );
 
+    const patterns = BASE_PATTERNS.slice();
+
+    if (prevPaths.length > 0) {
+      patterns.push({
+        group: prevPaths.map(globAlias),
+        message: 'Access to this layer from the current one is denied',
+      });
+    }
+
     return {
-      files: paths.map(globAlias),
+      files: paths.map((path) => `./{,src/}${path}/**/*`),
       rules: {
-        'no-restricted-import': [
-          'error',
-          {
-            patterns: [
-              {
-                group: prevPaths.map(globAlias),
-                message: 'Access to this layer from the current one is denied',
-              },
-              {
-                group: DEPRECATED_GLOB_ALIASES,
-                message: `Layer is deprecated. Instead, use layers related to FSD version 2.X.X: ${RELATED_LAYER_NAME_LIST}`,
-              },
-              {
-                group: BREAKING_GLOB_ALIASES,
-                message:
-                  'Using a relative and absolute paths may result in using an inaccessible layers. Use an aliased path instead',
-              },
-              {
-                group: UNKNOWN_GLOB_ALIASES,
-                message: `Unknown layer. Use layers related to FSD version 2.X.X: ${RELATED_LAYER_NAME_LIST}`,
-              },
-            ],
-          },
-        ],
+        'no-restricted-imports': ['error', { patterns }],
       },
     };
   }),
